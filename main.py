8caafc886c8e236dc312dc41bccea0a2987f05b7
Nate <natefonseka@gmail.com>
2013-01-27 12:13:22 -0500

    initial push

---
+++ main.py
@@ -0,0 +1,25 @@
+#Cupid Shuffle
+#Author: Nate Fonseka
+#Released under GNU GPL v3
+#Github: lilnate22/cupidsshuffle
+import pygame, sys,random, time
+from pygame.locals import *
+
+
+#basic pygame stuff
+
+
+def main():
+	pass;
+	
+	
+def runGame():
+	pass;
+	
+	
+def makeTextObj(text, font, color):
+	pass;
+	
+	
+def end():
+	pass;

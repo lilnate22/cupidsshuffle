1eb3c713f96d7d39db530dce21635a4fb25b95ae
Nate <natefonseka@gmail.com>
2013-01-27 14:11:03 -0500

    adding some basic game logic

--- main.py
+++ main.py
@@ -7,21 +7,120 @@ from pygame.locals import *
 from Cupid import *
 
 
-#basic pygame stuff
+#basic pygame stuff, taken from pygame tutorials. We need this to color things on screen
+FPS = 25
+WINDOWWIDTH = 800
+WINDOWHEIGHT = 600
+BOXSIZE = 20
+BOARDWIDTH = 20
+BOARDHEIGHT = 40
+BLANK = '.'
 
+XMARGIN = int((WINDOWWIDTH - BOARDWIDTH * BOXSIZE) / 2)
+TOPMARGIN = WINDOWHEIGHT - (BOARDHEIGHT * BOXSIZE) - 5
+
+#               R    G    B
+WHITE       = (255, 255, 255)
+GRAY        = (185, 185, 185)
+BLACK       = (  0,   0,   0)
+RED         = (155,   0,   0)
+LIGHTRED    = (175,  20,  20)
+GREEN       = (  0, 155,   0)
+LIGHTGREEN  = ( 20, 175,  20)
+BLUE        = (  0,   0, 155)
+LIGHTBLUE   = ( 20,  20, 175)
+YELLOW      = (155, 155,   0)
+LIGHTYELLOW = (175, 175,  20)
+
+BORDERCOLOR = BLUE
+BGCOLOR = BLACK
+TEXTCOLOR = WHITE
+TEXTSHADOWCOLOR = GRAY
+COLORS      = (     BLUE,      GREEN,      RED,      YELLOW)
+LIGHTCOLORS = (LIGHTBLUE, LIGHTGREEN, LIGHTRED, LIGHTYELLOW)
+assert len(COLORS) == len(LIGHTCOLORS) 
+
+
+#end basic pygame stuff
+
+GAMETIME = 120
+
+MOVEDOWNFREQ = 0.25   #this will be increased as time goes
+
+#the move down freq will be increased every 30 seconds. the game time is max 120
 
 def main():
-	print 'hello world'
-	pass;
-	
-	
+    global FPSCLOCK, DISPLAYSURF, BASICFONT, BIGFONT
+    pygame.init()
+    FPSCLOCK = pygame.time.Clock()
+    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+    BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
+    BIGFONT = pygame.font.Font('freesansbold.ttf', 100)
+    pygame.display.set_caption('Cupids Shuffle')
+
+    showTextScreen('Cupid Shuffle')
+
+    while True: #running the game
+    	runGame();
+    	showTextScreen("Game over")
+
 def runGame():
-	pass;
+	#set up our global "game" variables
+	board = getBlankBoard()
+	score = 0  
+	points_per_obj = 100
+	multiplier = 1.5
 	
+#this is the game board we will be going
+def getBlankBoard():
+    board = []
+    for i in range(BOARDWIDTH):
+        board.append([BLANK] * BOARDHEIGHT)
+    return board
+
+
 	
-def makeTextObj(text, font, color):
-	pass;
+def makeTextObjs(text, font, color):
+    surf = font.render(text, True, color)
+    return surf, surf.get_rect()
 	
 	
 def end():
 	pass;
+
+def checkForQuit():
+	pass;
+
+def checkForKeyPress():
+	#check for any keypress, and update accordingly
+    checkForQuit()
+
+    for event in pygame.event.get([KEYDOWN, KEYUP]):
+        if event.type == KEYDOWN:
+            continue
+        return event.key
+    return None
+
+# this is a standard pygame function. modified a little to fit what i want
+def showTextScreen(text):
+    titleSurf, titleRect = makeTextObjs(text, BIGFONT, TEXTSHADOWCOLOR)
+    titleRect.center = (int(WINDOWWIDTH / 2), int(WINDOWHEIGHT / 2))
+    DISPLAYSURF.blit(titleSurf, titleRect)
+
+    # Draw the text
+    titleSurf, titleRect = makeTextObjs(text, BIGFONT, TEXTCOLOR)
+    titleRect.center = (int(WINDOWWIDTH / 2) - 3, int(WINDOWHEIGHT / 2) - 3)
+    DISPLAYSURF.blit(titleSurf, titleRect)
+
+    #tiny text
+    pressKeySurf, pressKeyRect = makeTextObjs('Press a key to play.', BASICFONT, TEXTCOLOR)
+    pressKeyRect.center = (int(WINDOWWIDTH / 2), int(WINDOWHEIGHT / 2) + 100)
+    DISPLAYSURF.blit(pressKeySurf, pressKeyRect)
+
+    while checkForKeyPress() == None:
+        pygame.display.update()
+        FPSCLOCK.tick()
+
+
+if __name__ == '__main__':
+    main()

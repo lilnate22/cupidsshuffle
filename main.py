75a305188d832d430d05ee0d29e3fad660700da8
Nate <natefonseka@gmail.com>
2013-01-27 20:53:36 -0500

    adding bg sounds

--- main.py
+++ main.py
@@ -13,9 +13,25 @@ WIN_W = 700
 class Game:
         def __init__(self):
                 pygame.init()
-                self.display = pygame.display.set_mode((WIN_H,WIN_W),0,32)
+                self.screen = pygame.display.set_mode((700,700),0,32)
                 self.font = pygame.font.SysFont('freesansbold.ttf',40)
                 #self.display.fill((0,0,0))
+                self.clock = pygame.time.Clock()
+                
+        def end(self):
+                running = True
+                time = 0 # we will need this if the game freezes up
+                while running:
+                        time = time + self.clock.tick()
+
+                        #we need the quit event from pygame
+                        for event in pygame.event.get():
+                                
+                                if event.type == pygame.QUIT:
+                                        sys.exit()
+                                elif event.type == pygame.KEYDOWN and time > 1500:  #timed with the keypress...dont know what is going on
+                                        running = False
+                        self.screen.fill((255,130,120))
         
 if __name__ == '__main__':
     newgame = Game()
